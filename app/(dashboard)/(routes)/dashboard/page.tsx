d8d4b9f15080ccedffc9df83869ce11765870c3c
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-10-03 13:09:50 -0400

    added clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
-    <Button>
+    <div>
       Test
-    </Button>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+    
   )
 }
