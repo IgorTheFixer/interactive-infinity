28b4c5e24a6ba34428453520623b9fd79b0560ab
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-10-03 16:09:15 -0400

    finished sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
     <div>
       Test
-      <UserButton afterSignOutUrl="/"/>
     </div>
     
   )
